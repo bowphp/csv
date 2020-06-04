a4ee2876a2700cfb9baa3393d37ca6cbf9d4e139
Franck DAKIA <dakiafranck@gmail.com>
2020-06-04 12:30:30 +0000

    initial commit

---
+++ src/CsvExporterTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bowphp\Csv;
+
+trait CsvExporterTrait
+{
+	/**
+	 * Define the CSV headers
+	 * 
+	 * @var array
+	 */
+	protected $cvs_headers = [];
+
+	/**
+	 * Export Csv
+	 * 
+	 * @return array
+	 */
+	public function exportToCsv()
+	{
+		// Make csv
+	}
+}
---
+++ src/helper.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!function_exists('bow_csv_formater')) {
+	function bow_csv_importer($model, array $header)
+	{
+
+	}
+}
